2c097d26e63ca66bcbf96aafd02eb212037ac90f
heitorAlbu <heitor.albuquerque@gmail.com>
2020-05-29 16:51:02 -0300

    implementacaoLogin

--- app/src/main/java/com/example/plotarmapa/LoginActivity.java
+++ app/src/main/java/com/example/plotarmapa/LoginActivity.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.plotarmapa.Database.DatabaseHelper;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.SignInAccount;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
     DatabaseHelper db;
     EditText txtEmail, txtSenha ;
     Button btnLogin, btnCadastro;
     Boolean permissao;
+    GoogleSignInClient mGoogleSignInClient;
+    int RC_SIGN_IN = 0;
+    SignInButton btnGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,25 @@ public class LoginActivity extends AppCompatActivity {
         txtSenha = (EditText)findViewById(R.id.txtSenhaLogin);
         btnLogin = (Button)findViewById(R.id.btnEntrar);
         btnCadastro  = (Button)findViewById(R.id.btnCadastrarLogin);
+        btnGoogle = (SignInButton)findViewById(R.id.btnGoogle);
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        btnGoogle.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                switch(view.getId()){
+                    case  R.id.btnGoogle:
+                        signIn();
+                    break;
+                }
+            }
+        });
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,5 +88,47 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+   @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if(account != null){
+            Intent intent = new Intent(LoginActivity.this, informarCepActivity.class);
+            startActivity(intent);
+        }
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Intent intent = new Intent(LoginActivity.this, informarCepActivity.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Erro", "signInResult:failed code=" + e.getStatusCode());
+        }
     }
 }
--- app/src/main/java/com/example/plotarmapa/informarCepActivity.java
+++ app/src/main/java/com/example/plotarmapa/informarCepActivity.java
@@ -1,18 +1,27 @@
 package com.example.plotarmapa;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.plotarmapa.GoogleGeoCoding.GoogleGeoCode;
 import com.example.plotarmapa.GoogleGeoCoding.GoogleGeoLatLng;
 import com.example.plotarmapa.GoogleGeoCoding.GoogleGeoResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +30,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.stream.Stream;
 
 public class informarCepActivity extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -31,7 +42,24 @@ public class informarCepActivity extends AppCompatActivity {
         final EditText cep = findViewById(R.id.etMain_cep);
         final String resposta ;
         final Button  btnBuscarCep = findViewById(R.id.btnMain_buscarCep);
+        final Button btnDeslogar = findViewById(R.id.btnDeslogar);
         final String[] coordenadas = new String[2];
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        btnDeslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 switch(view.getId()){
+                     case  R.id.btnDeslogar:
+                            signOut();
+                     break;
+                }
+            }
+        });
 
         btnBuscarCep.setOnClickListener(view -> {
             if (cep.getText().length() == 8 && cep.getText().toString() != ""){
@@ -59,5 +87,16 @@ public class informarCepActivity extends AppCompatActivity {
             startActivity(intent);
 
         });
+
     }
+    private void  signOut() {
+        mGoogleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>(){
+            public  void onComplete(@NonNull Task<Void> task){
+
+                Toast.makeText(informarCepActivity.this , "Deslogado com sucesso!", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
+
 }
